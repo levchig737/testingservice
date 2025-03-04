b3dcdaa75c00a599a50cd6de4a16b71ce43646b6
levchig737 <levchekarev3@gmail.com>
2025-03-04 16:23:35 +0300

    Hot fix

--- backend/api/deps.py
+++ backend/api/deps.py
@@ -1,6 +1,7 @@
-from db.base import get_async_session
+from db.base import get_session_stub, get_async_session
 from fastapi import Depends
 
+from main import app
 from services.test import TestService
 from services.question import QuestionService
 from services.answer import AnswerService
@@ -11,26 +12,31 @@ from services.completed_tests_users import CompletedTestsUsersService
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_test_service(session: AsyncSession = Depends(get_async_session)) -> TestService:
+async def get_test_service(session: AsyncSession = Depends(get_session_stub)) -> TestService:
     return TestService(session=session)
 
 
-async def get_question_service(session: AsyncSession = Depends(get_async_session)) -> QuestionService:
+async def get_question_service(session: AsyncSession = Depends(get_session_stub)) -> QuestionService:
     return QuestionService(session=session)
 
 
-async def get_answer_service(session: AsyncSession = Depends(get_async_session)) -> AnswerService:
+async def get_answer_service(session: AsyncSession = Depends(get_session_stub)) -> AnswerService:
     return AnswerService(session=session)
 
 
-async def get_right_answer_service(session: AsyncSession = Depends(get_async_session)) -> RightAnswerService:
+async def get_right_answer_service(session: AsyncSession = Depends(get_session_stub)) -> RightAnswerService:
     return RightAnswerService(session=session)
 
 
-async def get_user_service(session: AsyncSession = Depends(get_async_session)) -> UserService:
+async def get_user_service(session: AsyncSession = Depends(get_session_stub)) -> UserService:
     return UserService(session=session)
 
 
-async def get_completed_tests_users_service(session: AsyncSession = Depends(get_async_session)) \
+async def get_completed_tests_users_service(session: AsyncSession = Depends(get_session_stub)) \
         -> CompletedTestsUsersService:
     return CompletedTestsUsersService(session=session)
+
+
+# --- Dependency Injection (DI) через `app.dependency_overrides` --- #
+
+app.dependency_overrides[get_session_stub] = get_async_session
--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 import uvicorn
 
-
-
+from db.base import get_async_session, get_session_stub
 
 origins = [
     "*",
@@ -26,6 +25,10 @@ app.add_middleware(
 )
 
 # app.middleware("http")(exception_traceback_middleware)
+
+# --- Dependency Injection (DI) через `app.dependency_overrides` --- #
+
+app.dependency_overrides[get_session_stub] = get_async_session
 
 app.include_router(api_router)
 
